125989084a22e3636d649952414556d830809312
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2023-07-04 08:27:03 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,5 @@ echo "BUILD STATIC"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic 
 echo  "BUILD END"
