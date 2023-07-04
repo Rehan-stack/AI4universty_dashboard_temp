25cb90283035c7f3518a110a6530f88dddc4b309
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2023-07-04 08:24:55 +0500

    Update settings.py

--- writoai/settings.py
+++ writoai/settings.py
@@ -160,7 +160,7 @@ DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'PNG': ".png"}
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
